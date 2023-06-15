503eeff4938d48ef9e5e8fa8dcb638f3665a6fc3
leeyr0412 <leeyr0412@naver.com>
2023-06-15 23:49:53 +0900

    feat(member): TIC-19 멤버 Entity 생성중

---
+++ src/main/java/com/y2h/tinybox/client/member/Member.java
@@ -0,0 +1,12 @@
+package com.y2h.tinybox.client.member;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+// TODO: 2023-06-15 멤버 도메인 생성중 
+@Entity
+public class Member {
+
+    @Id
+    Long id;
+}
