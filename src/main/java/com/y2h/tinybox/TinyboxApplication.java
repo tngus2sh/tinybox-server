391e4ea924214fcab6236c0430d76df5f202d204
leeyr0412 <leeyr0412@naver.com>
2023-06-15 13:46:42 +0900

    init: setup project

---
+++ src/main/java/com/y2h/tinybox/TinyboxApplication.java
@@ -0,0 +1,13 @@
+package com.y2h.tinybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyboxApplication.class, args);
+	}
+
+}
