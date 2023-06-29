b5a89be262fa2ea58c44666e6fbd6e368d4bc5aa
tngus2sh <ssuhyeon98@gmail.com>
2023-06-29 23:02:09 +0900

    feat(movie) TIC-17-movie movieService annotation 추가 및 MovieServiceTest 생성

--- src/main/java/com/y2h/tinybox/movie/service/MovieService.java
+++ src/main/java/com/y2h/tinybox/movie/service/MovieService.java
@@ -1,4 +1,7 @@
 package com.y2h.tinybox.movie.service;
 
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
 public interface MovieService {
 }
--- src/main/java/com/y2h/tinybox/movie/service/impl/MovieServiceImpl.java
+++ src/main/java/com/y2h/tinybox/movie/service/impl/MovieServiceImpl.java
@@ -1,4 +1,10 @@
 package com.y2h.tinybox.movie.service.impl;
 
-public class MovieServiceImpl {
+import com.y2h.tinybox.movie.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MovieServiceImpl implements MovieService {
 }
