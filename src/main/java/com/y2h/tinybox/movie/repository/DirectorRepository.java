64d7f2b17701f4304609e05c620f419bb00e98b4
tngus2sh <ssuhyeon98@gmail.com>
2023-06-30 23:11:10 +0900

    feat(movie) TIC-17-movie DirectorRepository 생성

---
+++ src/main/java/com/y2h/tinybox/movie/repository/DirectorRepository.java
@@ -0,0 +1,7 @@
+package com.y2h.tinybox.movie.repository;
+
+import com.y2h.tinybox.movie.Director;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DirectorRepository extends JpaRepository<Director, Long> {
+}
